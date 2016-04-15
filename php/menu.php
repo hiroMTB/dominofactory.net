7b1b152f5435fd447b40b85e3cf360c55199d686
hiroMTB <matobahiroshi@gmail.com>
2016-04-16 00:44:02 +0200

    start using php template system

--- php/menu.php
+++ php/menu.php
@@ -1,8 +1,8 @@
 <div id="menu">
 	<ul>
 		<li><a href="http://dominofactory.net"> dominofactory.net</a> by hiroshi matoba</li>
-		<li><a href="bio.html">BIO</a></li>
-		<li><a href="info.html">INFO</a></li>
-		<li><a href="works.html">WORKS</a></li>
+		<li><a href="http://dominofactory.net/bio.html">BIO</a></li>
+		<li><a href="http://dominofactory.net/info.html">INFO</a></li>
+		<li><a href="http://dominofactory.net/works.html">WORKS</a></li>
 	</ul>
 </div>
---
+++ php/template_work.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
+	<head profile="http://gmpg.org/xfn/11">
+		<?php include_once("../php/head_include.php") ?>
+		<title><?php echo $title; ?></title>
+	</head>
+	<body>
+		<div id="container">
+			<?php include("../php/menu.php") ?>
+			<!-- Start contents -->		
+			<?php echo $contents; ?>
+			<!-- end of contents -->			
+			<?php include("../php/works_navi.php") ?>
+		</div>
+	</body> 
+</html> 
